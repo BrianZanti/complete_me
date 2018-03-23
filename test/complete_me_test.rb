b3600708e3c019e9deebcec4135c74860278abba
Brian Zanti <brianzanti@gmail.com>
2018-03-22 21:33:12 -0600

    adds insert test

--- test/complete_me_test.rb
+++ test/complete_me_test.rb
@@ -3,8 +3,30 @@ require 'minitest/pride'
 require_relative '../lib/complete_me'
 
 class CompleteMeTest < Minitest::Test
-  def test_it_exists
-    cm = CompleteMe.new
-    assert_instance_of CompleteMe, cm
+  def setup
+    @cm = CompleteMe.new
   end
+
+  def test_it_exists
+    assert_instance_of CompleteMe, @cm
+  end
+
+  def test_it_has_empty_root
+    @cm = CompleteMe.new
+    assert_nil @cm.root.value
+    assert_instance_of Node, @cm.root
+  end
+
+  def test_insert
+    @cm.insert("pizza")
+    leaf = @cm.root.child("p")
+                   .child("i")
+                   .child("z")
+                   .child("z")
+                   .child("a")
+    assert_instance_of Node, leaf
+    empty_children = {}
+    assert_equal empty_children, leaf.children
+  end
+
 end
