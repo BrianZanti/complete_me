46bdcab3649cb234d195ba7b8e6b69674f001465
Brian Zanti <brianzanti@gmail.com>
2018-03-26 17:10:01 -0600

    removes test for case sensitiviy. Refactors into setup

--- test/complete_me_test.rb
+++ test/complete_me_test.rb
@@ -6,6 +6,13 @@ require 'pry'
 class CompleteMeTest < Minitest::Test
   def setup
     @cm = CompleteMe.new
+    @cm.insert("pizza")
+    @cm.insert("pizzapizza")
+    @cm.insert("apple")
+    @cm.insert("pizzicato")
+    @cm.insert("pizzle")
+    @cm.insert("banana")
+    @cm.insert("aardvark")
   end
 
   def test_it_exists
@@ -13,14 +20,15 @@ class CompleteMeTest < Minitest::Test
   end
 
   def test_it_has_empty_root
-    @cm = CompleteMe.new
-    assert_nil @cm.root.value
-    assert_instance_of Node, @cm.root
+    cm = CompleteMe.new
+    assert_nil cm.root.value
+    assert_instance_of Node, cm.root
   end
 
   def test_insert
-    @cm.insert("pizza")
-    leaf = @cm.root.child("p")
+    cm = CompleteMe.new
+    cm.insert("pizza")
+    leaf = cm.root.child("p")
                    .child("i")
                    .child("z")
                    .child("z")
@@ -31,58 +39,40 @@ class CompleteMeTest < Minitest::Test
     assert_equal empty_children, leaf.children
   end
 
+  def test_count
+    assert_equal 7, @cm.count
+  end
+
   def test_inserting_duplicates_has_no_effect
     @cm.insert("pizza")
-    @cm.insert("Pizza")
-    assert_equal 1, @cm.count
+    assert_equal 7, @cm.count
   end
 
   def test_inserting_superstring_words_will_maintain_substring_as_word
-    @cm.insert("pizza")
     @cm.insert("pizzapizza")
     pizza = @cm.root.child("p")
                     .child("i")
                     .child("z")
                     .child("z")
                     .child("a")
+
     assert pizza.word?
+
     pizzapizza = pizza.child("p")
                       .child("i")
                       .child("z")
                       .child("z")
                       .child("a")
+
     assert pizzapizza.word?
     empty_children = {}
     assert_equal empty_children, pizzapizza.children
   end
 
-  def test_values_are_lower_cased
-    @cm.insert("PIZZA")
-    leaf = @cm.root.child("p")
-                   .child("i")
-                   .child("z")
-                   .child("z")
-                   .child("a")
-    assert_instance_of Node, leaf
-    empty_children = {}
-    assert_equal empty_children, leaf.children
-  end
-
-  def test_count
-    @cm.insert("pizza")
-    @cm.insert("pizzapizza")
-    @cm.insert("apple")
-    @cm.insert("pizzicato")
-    @cm.insert("pizzle")
-    @cm.insert("banana")
-    @cm.insert("aardvark")
-    assert_equal 7, @cm.count
-  end
-
   def test_populate
-    words = "A\na\naa\naal\naalii\naam\nAani\naardvark\naardwolf\nAaron\n"
+    words = "A\na\naa\naal\naalii\naam\nAani\naardwolf\nAaron\n"
     @cm.populate(words)
-    assert_equal 9, @cm.count
+    assert_equal 16, @cm.count
   end
 
   def test_find
@@ -98,6 +88,11 @@ class CompleteMeTest < Minitest::Test
   end
 
   def test_suggest
+    suggestions = ["pizza", "pizzapizza", "pizzicato", "pizzle"]
+    assert_equal suggestions, @cm.suggest("piz")
+  end
+
+  def test_suggest_whole_dictionary
     skip
     @cm.populate(File.read("/usr/share/dict/words"))
     suggestions = ["pize", "pizza", "pizzeria", "pizzicato", "pizzle"]
