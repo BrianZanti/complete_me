4e52a193ccd69009ced84295d0acf44ec3518b8a
Brian Zanti <brianzanti@gmail.com>
2018-03-26 21:15:51 -0600

    adds tests for suggest

--- test/complete_me_test.rb
+++ test/complete_me_test.rb
@@ -7,6 +7,7 @@ class CompleteMeTest < Minitest::Test
   def setup
     @cm = CompleteMe.new
     @cm.insert("pizza")
+    @cm.insert("pize")
     @cm.insert("pizzapizza")
     @cm.insert("apple")
     @cm.insert("pizzicato")
@@ -40,12 +41,12 @@ class CompleteMeTest < Minitest::Test
   end
 
   def test_count
-    assert_equal 7, @cm.count
+    assert_equal 8, @cm.count
   end
 
   def test_inserting_duplicates_has_no_effect
     @cm.insert("pizza")
-    assert_equal 7, @cm.count
+    assert_equal 8, @cm.count
   end
 
   def test_inserting_superstring_words_will_maintain_substring_as_word
@@ -72,14 +73,12 @@ class CompleteMeTest < Minitest::Test
   def test_populate
     words = "A\na\naa\naal\naalii\naam\nAani\naardwolf\nAaron\n"
     @cm.populate(words)
-    assert_equal 16, @cm.count
+    assert_equal 17, @cm.count
   end
 
   def test_find
-    @cm.insert("pizza")
-    @cm.insert("pizzicato")
-    @cm.insert("pizzle")
-    found_node = @cm.find("pizz")
+    fragment = "pizz"
+    found_node = @cm.find(fragment)
     assert_instance_of Node, found_node
     assert_instance_of Node, found_node.child("l")
     assert_instance_of Node, found_node.child("a")
@@ -87,15 +86,39 @@ class CompleteMeTest < Minitest::Test
     assert_nil found_node.child("e")
   end
 
+  def test_methods_arent_destructive
+  end
+
+  def test_traverse
+    start_node = @cm.root.child("p")
+                    .child("i")
+                    .child("z")
+                    .child("z")
+
+    traversals = @cm.traverse(start_node, "pizz")
+    assert traversals.include? "pizzapizza"
+    assert traversals.include? "pizzicato"
+    assert traversals.include? "pizzle"
+
+  end
+
   def test_suggest
+    suggestions = ["pize", "pizza", "pizzapizza", "pizzicato", "pizzle"]
+    suggestions.each do |suggestion|
+        assert @cm.suggest("piz").include? suggestion
+    end
     suggestions = ["pizza", "pizzapizza", "pizzicato", "pizzle"]
-    assert_equal suggestions, @cm.suggest("piz")
+    suggestions.each do |suggestion|
+        assert @cm.suggest("pizz").include? suggestion
+    end
   end
 
   def test_suggest_whole_dictionary
     skip
     @cm.populate(File.read("/usr/share/dict/words"))
     suggestions = ["pize", "pizza", "pizzeria", "pizzicato", "pizzle"]
-    assert_equal suggestions, @cm.suggest("piz")
+    suggestions.each do |suggestion|
+        assert @cm.suggest("piz").include? suggestion
+    end
   end
 end
