97579195015d4cf348818009aaa3718bd43fe9dd
Brian Zanti <brianzanti@gmail.com>
2018-03-26 22:41:54 -0600

    adds tests for usage weighting

--- test/complete_me_test.rb
+++ test/complete_me_test.rb
@@ -14,6 +14,18 @@ class CompleteMeTest < Minitest::Test
     @cm.insert("pizzle")
     @cm.insert("banana")
     @cm.insert("aardvark")
+
+    @cm.select("piz", "pizzeria")
+    @cm.select("piz", "pizzeria")
+    @cm.select("piz", "pizzeria")
+
+    @cm.select("pi", "pizza")
+    @cm.select("pi", "pizza")
+    @cm.select("pi", "pizza")
+    @cm.select("pi", "pizza")
+    @cm.select("pi", "pizzapizza")
+    @cm.select("pi", "pizzicato")
+    @cm.select("pi", "pizzicato")
   end
 
   def test_it_exists
@@ -26,14 +38,22 @@ class CompleteMeTest < Minitest::Test
     assert_instance_of Node, cm.root
   end
 
+  def test_it_has_selections_with_default_values
+    cm = CompleteMe.new
+    empty_hash = {}
+    assert_equal empty_hash, cm.selections
+    assert_equal empty_hash, cm.selections[:any_key]
+    assert_equal 0, cm.selections[:any_key][:any_key]
+  end
+
   def test_insert
     cm = CompleteMe.new
     cm.insert("pizza")
     leaf = cm.root.child("p")
-                   .child("i")
-                   .child("z")
-                   .child("z")
-                   .child("a")
+    .child("i")
+    .child("z")
+    .child("z")
+    .child("a")
     assert_instance_of Node, leaf
     assert leaf.word?
     empty_children = {}
@@ -52,18 +72,18 @@ class CompleteMeTest < Minitest::Test
   def test_inserting_superstring_words_will_maintain_substring_as_word
     @cm.insert("pizzapizza")
     pizza = @cm.root.child("p")
-                    .child("i")
-                    .child("z")
-                    .child("z")
-                    .child("a")
+    .child("i")
+    .child("z")
+    .child("z")
+    .child("a")
 
     assert pizza.word?
 
     pizzapizza = pizza.child("p")
-                      .child("i")
-                      .child("z")
-                      .child("z")
-                      .child("a")
+    .child("i")
+    .child("z")
+    .child("z")
+    .child("a")
 
     assert pizzapizza.word?
     empty_children = {}
@@ -91,9 +111,9 @@ class CompleteMeTest < Minitest::Test
 
   def test_traverse
     start_node = @cm.root.child("p")
-                    .child("i")
-                    .child("z")
-                    .child("z")
+    .child("i")
+    .child("z")
+    .child("z")
 
     traversals = @cm.traverse(start_node, "pizz")
     assert traversals.include? "pizzapizza"
@@ -105,12 +125,46 @@ class CompleteMeTest < Minitest::Test
   def test_suggest
     suggestions = ["pize", "pizza", "pizzapizza", "pizzicato", "pizzle"]
     suggestions.each do |suggestion|
-        assert @cm.suggest("piz").include? suggestion
+      assert @cm.suggest("piz").include? suggestion
     end
     suggestions = ["pizza", "pizzapizza", "pizzicato", "pizzle"]
     suggestions.each do |suggestion|
-        assert @cm.suggest("pizz").include? suggestion
+      assert @cm.suggest("pizz").include? suggestion
     end
+  end
+
+  def test_select
+    expected =  {
+      piz: {
+        pizzeria: 3
+      },
+      pi: {
+        pizza: 4,
+        pizzapizza: 1,
+        pizzicato: 2
+      }
+    }
+    assert_equal expected, @cm.selections
+  end
+
+  def test_weigh_selections
+    selections = @cm.weigh_selections("pi")
+    assert_equal ["pizza", "pizzicato", "pizzapizza"], selections
+  end
+
+  def test_usage_weighting
+    suggestions = @cm.suggest("piz")
+    assert_equal "pizzeria", suggestions[0]
+    assert_equal 6, suggestions.length
+
+    suggestions = @cm.suggest("pi")
+    assert_equal "pizza", suggestions[0]
+    assert_equal "pizzicato", suggestions[1]
+    assert_equal "pizzapizza", suggestions[2]
+    assert_equal 5, suggestions.length
+  end
+
+  def test_selecting_should_add_to_dictionary
   end
 
   def test_suggest_whole_dictionary
@@ -118,7 +172,7 @@ class CompleteMeTest < Minitest::Test
     @cm.populate(File.read("/usr/share/dict/words"))
     suggestions = ["pize", "pizza", "pizzeria", "pizzicato", "pizzle"]
     suggestions.each do |suggestion|
-        assert @cm.suggest("piz").include? suggestion
+      assert @cm.suggest("piz").include? suggestion
     end
   end
 end
